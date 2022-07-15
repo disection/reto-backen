5ffe9283a6d80c548bb78da93397e877e2310cf1
disection <diseccionmexico@gmail.com>
2022-07-14 20:12:02 -0500

    listed server

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+const server = require("./src/server")
+
+// deconstrucción
+const {
+    DB_USER,
+  DB_PASSWORD, 
+  DB_HOST, 
+  DB_NAME
+} = process.env
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
+.then( () => {
+    console.log( "Conneted to DB" )
+
+    server.listen(8080, (request, response)=>{
+        console.log("Nuestro servidor esta encendido")
+    })
+})
+.catch( (error) => {
+
+    console.log("Hubo un error", error )
+
+})
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+
+const app = express()
+
+
+// Middleware
+
+
+// Middleware de rutas
+
+
+//Endpoint HOME
+
+
+// Exportar
+
+module.exports = app
