fd68dbcbfcc1a9a1a01c9373366bf5c0e49245e0
disection <diseccionmexico@gmail.com>
2022-07-14 21:09:48 -0500

    MAKE schemas post

---
+++ src/models/post.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+// Schemas
+
+const postSchema = new moongoose.Schema({
+    tittle: { 
+        type: String, 
+        required: true , 
+        unique: true
+    },
+    image: { 
+        type: String,
+        required: true 
+    },
+    content: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        index: true 
+    },
+    date: { 
+        type: String, 
+        required: true 
+    },
+    tags: { 
+        type: String, 
+        required: true,  
+    },
+    author: {
+        type: String,
+        require: true,
+        maxlength: 20,
+        minlength: 3
+    }
+
+    
+})
+
+module.exports = mongoose.model("post", postSchema)
